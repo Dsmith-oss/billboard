32231ea8c38a0a39b9565ea07e09062a663ab1bd
Declan Smith <dmansmith03@yahoo.com>
2021-02-23 15:10:00 -0600

    Initial Commit

---
+++ billboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  billboard
+//
+//  Created by Declan Smith on 2/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ billboard/billboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  billboardApp.swift
+//  billboard
+//
+//  Created by Declan Smith on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct billboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
